85eae7f1fd973beb430c76e4eb9c92a7c4eff949
Norgerman <xyn0410@gmail.com>
2017-04-13 14:20:40 +0800

    fix powshell

--- build/createConf.ps1
+++ build/createConf.ps1
@@ -1,3 +1,11 @@
-echo "export var appid = '';" >> app\xunfei\conf.ts
-echo "export var appkey = '';" >> app\xunfei\conf.ts
-cat app\xunfei\conf.ts
+$encoding = New-Object System.Text.UTF8Encoding $False
+$content = "export var appid = ''", "export var appkey = ''"
+$path = Join-Path (Get-Item -Path ".\").FullName "app\xunfei\conf.ts"
+$stream = New-Object System.IO.FileStream($path, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
+$writer = New-Object System.IO.StreamWriter($stream, $encoding)
+foreach ($i in $content) 
+{
+    $writer.WriteLine($i)
+}
+$writer.dispose();
+$stream.dispose();
