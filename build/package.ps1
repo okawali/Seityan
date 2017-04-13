29e1566551eae9fd40f617643ed1f6aa86ab693e
Norgerman <xyn0410@gmail.com>
2017-04-13 14:24:03 +0800

    in path

--- build/package.ps1
+++ build/package.ps1
@@ -1,3 +1,5 @@
 Add-Type -A System.IO.Compression.FileSystem
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'release-builds\electron-live2d-win32-x64'
+$outPath = Join-Path (Get-Item -Path ".\").FullName 'release-builds\electron-live2d-win32-x64.zip'
 npm run package-win32-x64
-[IO.Compression.ZipFile]::CreateFromDirectory('release-builds\electron-live2d-win32-x64', 'release-builds\electron-live2d-win32-x64.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath)
