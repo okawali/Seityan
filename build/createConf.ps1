d2c930d5a131b69966796b569882093c6492073c
Norgerman <xyn0410@gmail.com>
2017-04-14 18:58:41 +0800

    i know

--- build/createConf.ps1
+++ build/createConf.ps1
@@ -1,11 +1,10 @@
 $encoding = New-Object System.Text.UTF8Encoding $False
-$content = [System.String]::Format("export var appid = '{0}'", $env:XUNFEI_APP_ID), [System.String]::Format("export var appkey = '0'", $env:XUNFEI_APP_KEY)
+$content = [System.String]::Format("export var appid = '{0}'", $env:XUNFEI_APP_ID), [System.String]::Format("export var appkey = '{0}'", $env:XUNFEI_APP_KEY)
 $path = Join-Path (Get-Item -Path ".\").FullName "app\xunfei\conf.ts"
 $stream = New-Object System.IO.FileStream($path, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
 $writer = New-Object System.IO.StreamWriter($stream, $encoding)
 foreach ($i in $content) 
 {
-    [System.Console]::WriteLine($i)
     $writer.WriteLine($i)
 }
 $writer.dispose();
