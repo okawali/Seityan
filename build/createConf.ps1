82ad5c9271a8c484c0c3c990e5e07b4638dd5b89
Norgerman <xyn0410@gmail.com>
2017-04-14 21:04:49 +0800

    add comf

--- build/createConf.ps1
+++ build/createConf.ps1
@@ -1,5 +1,9 @@
 $encoding = New-Object System.Text.UTF8Encoding $False
-$content = [System.String]::Format("export var appid = '{0}'", $env:XUNFEI_APP_ID), [System.String]::Format("export var appkey = '{0}'", $env:XUNFEI_APP_KEY)
+$content = [System.String]::Format("export var xunfeiAppId = '{0}'", $env:XUNFEI_APP_ID), `
+    [System.String]::Format("export var xunfeiAppKey = '{0}'", $env:XUNFEI_APP_KEY), `
+    [System.String]::Format("export var faceppKey = '{0}'", $env:FACEPP_KEY), `
+    [System.String]::Format("export var faceppSecret = '{0}'", $env:FACEPP_SECRET), `
+    [System.String]::Format("export var yyyKey = '{0}'", $env:YYY_KEY)
 $path = Join-Path (Get-Item -Path ".\").FullName "app\xunfei\conf.ts"
 $stream = New-Object System.IO.FileStream($path, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
 $writer = New-Object System.IO.StreamWriter($stream, $encoding)
--- build/createConf.sh
+++ build/createConf.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "export var appid = '${XUNFEI_APP_ID}'" >> app/xunfei/conf.ts
-echo "export var appkey = '${XUNFEI_APP_KEY}'" >> app/xunfei/conf.ts
+echo "export var xunfeiAppId = '${XUNFEI_APP_ID}'" >> app/utils/conf.ts
+echo "export var xunfeiAppKey = '${XUNFEI_APP_KEY}'" >> app/utils/conf.ts
+echo "export var faceppKey = '${FACEPP_KEY}'" >> app/utils/conf.ts
+echo "export var faceppSecret = '${FACEPP_SECRET}'" >> app/utils/conf.ts
+echo "export var yyyKey = '${YYY_KEY}'" >> app/utils/conf.ts
