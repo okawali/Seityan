80e6474761cc045028f48d686d5cf2029b4c4edb
Norgerman <xyn0410@gmail.com>
2017-04-14 21:08:41 +0800

    fix file name

--- build/createConf.ps1
+++ build/createConf.ps1
@@ -4,7 +4,7 @@ $content = [System.String]::Format("export var xunfeiAppId = '{0}'", $env:XUNFEI
     [System.String]::Format("export var faceppKey = '{0}'", $env:FACEPP_KEY), `
     [System.String]::Format("export var faceppSecret = '{0}'", $env:FACEPP_SECRET), `
     [System.String]::Format("export var yyyKey = '{0}'", $env:YYY_KEY)
-$path = Join-Path (Get-Item -Path ".\").FullName "app\xunfei\conf.ts"
+$path = Join-Path (Get-Item -Path ".\").FullName "app\utils\conf.ts"
 $stream = New-Object System.IO.FileStream($path, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
 $writer = New-Object System.IO.StreamWriter($stream, $encoding)
 foreach ($i in $content) 
