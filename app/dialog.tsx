60af458fc4a192cf6269d121ae80f3f629220703
西风逍遥游 <sunxfancy@gmail.com>
2017-04-20 22:00:27 +0800

    add dialog and remove its overlay

--- app/dialog.tsx
+++ app/dialog.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton  from "material-ui/RaisedButton"
+import {RaisedButton, FlatButton, Dialog} from "material-ui"
 import * as injectTapEventPlugin from "react-tap-event-plugin"
 
 injectTapEventPlugin();
@@ -9,12 +9,50 @@ injectTapEventPlugin();
 class App extends React.Component<{}, {}> {
     constructor(props?: {}, context?: any) {
         super(props, context);
+        this.handleClose = this.handleClose.bind(this)
+        this.handleOpen = this.handleOpen.bind(this)
     }
+    state = {
+        open: false,
+    };
+
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
 
     render() {
+        const actions = [
+        <FlatButton
+            label="Cancel"
+            primary={true}
+            onTouchTap={this.handleClose}
+        />,
+        <FlatButton
+            label="Submit"
+            primary={true}
+            disabled={true}
+            onTouchTap={this.handleClose}
+        />,
+        ];
+
         return (
             <MuiThemeProvider>
-                <RaisedButton label="click me" />
+                <div>
+                    <RaisedButton label="click me" onTouchTap={this.handleOpen} />
+                    <Dialog
+                        title="Dialog With Actions"
+                        actions={actions}
+                        modal={true}
+                        open={this.state.open}
+                        overlayStyle={{background: null}}
+                    >
+                        Only actions can close this dialog.
+                    </Dialog>
+                </div>
             </MuiThemeProvider>
         );
     }
