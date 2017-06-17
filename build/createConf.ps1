775b754d78181d00044d33a2c839f7e8b88276f5
徐宇楠 <xyn0410@gmail.com>
2017-06-17 16:14:01 +0800

    Update createConf.ps1

--- build/createConf.ps1
+++ build/createConf.ps1
@@ -5,6 +5,5 @@ If (Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER)
 Else 
 {
     nuget install secure-file -ExcludeVersion
-    echo $env:SECRET_FILE_KEY
     .\secure-file\tools\secure-file.exe -decrypt build\conf.ts.win.enc -secret $env:SECRET_FILE_KEY -out app\utils\conf.ts
 }
